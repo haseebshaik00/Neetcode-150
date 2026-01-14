79ee9cbd321d29387d517aa8c0f992c53075c908
haseebshaik00 <haseebshaik00@gmail.com>
2026-01-14 13:42:12 -0800

    Back to grind

--- Codes/1-Arrays-Hashing/5-LC347-Top-k-freq-elements.cpp
+++ Codes/1-Arrays-Hashing/5-LC347-Top-k-freq-elements.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     // Time: O(n) | Space: O(n)
-    // Approach 2: Time: O(n log k) | Space: O(n)
+    // Approach 2: Time: O(n log k) | Space: O(n) - using priority queue
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
-        for(auto &x: nums) freq[x]++;
-        vector<vector<int>> v(nums.size()+1);
-        for(auto &x: freq) v[x.second].push_back(x.first);
+        int n = nums.size();
         vector<int> ans;
-        for(int i=nums.size(); i>=0; --i){
+        unordered_map<int, int> freq;
+        vector<unordered_set<int>> v(n+1);
+        for(auto &x: nums) ++freq[x];
+        for(auto &x: freq) v[x.second].insert(x.first);
+        for(int i=n; i>=0; --i){
             for(auto &x: v[i]){
                 ans.push_back(x);
                 if(ans.size() == k) return ans;
