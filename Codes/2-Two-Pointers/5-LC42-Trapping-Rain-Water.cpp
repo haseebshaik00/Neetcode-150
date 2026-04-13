1751298a96d98688761aacc6ce5eb149206bbef1
haseebshaik00 <haseebshaik00@gmail.com>
2026-04-13 17:18:28 +0530

    Day 2 - DSA Grind

--- Codes/2-Two-Pointers/5-LC42-Trapping-Rain-Water.cpp
+++ Codes/2-Two-Pointers/5-LC42-Trapping-Rain-Water.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     // Time: O(n) | Space: O(1)
     int trap(vector<int>& height) {
-        int l=0, r=height.size()-1, ans = 0;
-        int lmax=height[l], rmax=height[r];
+        int l=0, r=height.size()-1, ans=0;
+        int lmax=height[0], rmax=height[r];
         while(l <= r){
             if(lmax < rmax){
                 ans += max(0, lmax-height[l]);
-                lmax = max(lmax, height[l]);
-                ++l;
-            } else {
+                lmax = max(lmax, height[l++]);
+            }
+            else{
                 ans += max(0, rmax-height[r]);
-                rmax = max(rmax, height[r]);
-                --r;
+                rmax = max(rmax, height[r--]);
             }
         }
         return ans;
